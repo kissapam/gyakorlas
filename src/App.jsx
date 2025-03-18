80bc9ab8f4649c15f69b24b94a7ef725c5509c3b
kissapam <kissrobilaci@gmail.com>
2025-03-18 17:46:52 +0100

    masodik

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
 
   return (
     <>
-      <div></div>
+      <div>
+        <img src="" alt="" />
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
